d67e4ad8370897048e73db443fc13bbf8557fe9f
madalenablanc <madalenablanc@ua.pt>
2023-08-31 01:51:06 +0100

    edited fom_with limits code

--- FOM_withlimits.py
+++ FOM_withlimits.py
@@ -56,7 +56,7 @@ def get_signal_composite(v,sel):
 
 
 
-def calc_fom(v,signal,background,minv,maxv,fs,fb,show_upper):
+def calc_fom(v,signal,background,minv,maxv,fs,fb,show_upper,legend):
     os.chdir("/user/u/u23madalenablanc/flavour-anomalies/SummerLIP23/"+folder)
 
     num_points=100
@@ -99,25 +99,39 @@ def calc_fom(v,signal,background,minv,maxv,fs,fb,show_upper):
         else:
             fom_opposite.append(0)
 
+
+    if show_upper==1:
+        label="upper cut"
+        fom_to_plot=fom_opposite
+    else:
+        label="lower cut"
+        fom_to_plot=fom
+
+
+    os.chdir("/user/u/u23madalenablanc/flavour-anomalies/plot_fom/")
+
     plt.figure()
     
-    if show_upper==1:
-        plt.plot(var_range,fom_opposite,label="upper cut")
-    else :
-        plt.plot(var_range,fom,label="lower cut")
-    plt.title(f"FOM for: {v}")
+    
+    plt.plot(var_range,fom_to_plot,label=label)
+
+    #plt.title(f"FOM for: {v}")
     plt.legend()
+    plt.xlabel(legend)
+    plt.ylabel("FOM")
+
     while True:
         try:
-            plt.savefig(v+'hist.png')
+            plt.savefig(v+'_fom.png')
             break
         except FileNotFoundError:
             print("bad name")
-            v=v.replace("/", "_div_")
+            v=v.replace("/", "_div_")    
 
 
 
-left_edge,right_edge,fb,fs=utils_fom.get_factors("/user/u/u23madalenablanc/SummerLIP23/Fit results/B0Fit_3.5sigma_results.txt")
+
+left_edge,right_edge,fb,fs=utils_fom.get_factors("/user/u/u23madalenablanc/flavour-anomalies/SummerLIP23/Fit results/B0Fit_3.5sigma_results.txt")
 
 sel="(tagged_mass<" + left_edge+ ") | (tagged_mass>" +right_edge + ")"
 
@@ -131,9 +145,10 @@ for v in df["var_name"]:
     minv= df.loc[df["var_name"] == v, "min"].iloc[0]
     maxv= df.loc[df["var_name"] == v, "max"].iloc[0]
     show_upper=df.loc[df["var_name"] == v, "show_upper"].iloc[0]
+    legend=df.loc[df["var_name"] == v, "legend"].iloc[0]
     print(minv,maxv)
     if composite_value==0:
         signal,back=get_signal_normal(v,sel)
     elif (composite_value)==1:
         signal,back=get_signal_composite(v,sel)
-    calc_fom(v,signal,back,minv,maxv,fs,fb,show_upper)
+    calc_fom(v,signal,back,minv,maxv,fs,fb,show_upper,legend)
